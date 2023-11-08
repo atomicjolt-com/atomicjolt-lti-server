fc309b538595e84a981b14cb78dcd3eaa8b8b371
Justin Ball <justinball@gmail.com>
2023-11-08 15:32:08 -0700

    Moved validateNonce into lib

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
-export { buildInit } from './libs/oidc';
+export { buildInit, validateNonce } from './libs/oidc';
 export { getLtiStorageParams } from './libs/platforms';
 export { validateIdTokenContents } from './libs/lti_validation';
 export {
-  OPEN_ID_COOKIE_PREFIX, 
+  OPEN_ID_COOKIE_PREFIX,
   OPEN_ID_STORAGE_COOKIE,
 } from './libs/constants';
 
