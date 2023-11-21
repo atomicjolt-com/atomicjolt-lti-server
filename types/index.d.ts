14de964940e25fb5e84a47df00f1086d17903389
Justin Ball <justinball@gmail.com>
2023-11-21 16:03:49 -0700

    Updated versions

--- types/index.d.ts
+++ types/index.d.ts
@@ -6,7 +6,6 @@ export { ALGORITHM, signJwt, verifyJwt, getKid, getIss } from './libs/jwt';
 export { generateKeySet, keySetsToJwks, fetchRemoteJwks, verifyJwtUsingJwks } from './libs/jwks';
 export { TEST_ID_TOKEN, genJwt } from './tests/helper';
 export { getDefaultToolConfiguration } from './libs/tool_configuration';
-export { getDefaultToolJwt } from './libs/tool_jwt';
 export { parseLinkHeader } from './libs/link_header';
 export { requestServiceToken, ClientCredentialsError } from './libs/client_credentials';
 //# sourceMappingURL=index.d.ts.map
